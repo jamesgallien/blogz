b46787cf0b0be1af63517d1aa0125258179f986a
James Gallien <jamesgallien@yahoo.com>
2017-10-10 20:13:15 -0500

    fuctionality: keep value in txt box when other is empty

--- main.py
+++ main.py
@@ -32,47 +32,27 @@ def newpost():
         blog_entry = request.form['entry']
         if len(blog_title) == 0 or len(blog_entry) == 0:
             flash("Both fields are required", "error")
-            return redirect('/newpost')
+            return render_template('newpost.html', blog_title=blog_title, blog_entry=blog_entry, title="Add a Blog Entry")
 
         new_blog = Blog(blog_title, blog_entry)    
         db.session.add(new_blog)    
         db.session.commit()
         id = new_blog.id
-
         return redirect('/blog?id=' + str(id))
 
-        # blogs = Blog.query.all() # filter_by(completed=False)    
-        # # completed_tasks = Task.query.filter_by(completed=True).all()
-        # return render_template('blog.html',title="Build a Blog",     
-        #         blogs=blogs) # completed_tasks=completed_tasks)    
-    
     return render_template('newpost.html', title="Add a Blog Entry")
 
 @app.route('/blog', methods=['GET'])
 def blog():   
     
-    # completed_tasks = Task.query.filter_by(completed=True).all()
     if request.args:
         blog = Blog.query.filter_by(id=request.args.get('id')).first()
         blog_title = blog.title
         blog_entry = blog.entry
         return render_template('blog.html', blog_title=blog_title, blog_entry=blog_entry, title=blog_title)
 
-    blogs = Blog.query.all() # filter_by(completed=False)
-    return render_template('blog.html', blogs=blogs, title="Build a Blog") # completed_tasks=completed_tasks)
-
-
-# @app.route('/delete-task', methods=['POST'])
-# def delete_task():
-
-#     task_id = int(request.form['task-id'])
-#     task = Task.query.get(task_id)
-#     task.completed = True
-#     db.session.add(task)
-#     db.session.commit()
-
-#     return redirect('/')
-
+    blogs = Blog.query.all()
+    return render_template('blog.html', blogs=blogs, title="Build a Blog")
 
 if __name__ == '__main__':
     app.run()
